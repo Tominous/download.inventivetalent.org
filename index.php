a86b6ca7ef2b20b90e2162e2927aee75231255aa
inventivetalent <inventivetalent@inventivegames.de>
2017-02-04 09:20:37 +0100

    update boostrap theme

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <title>Download</title>
 
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
 
     <meta name=viewport content="width=device-width, initial-scale=1">
     <base href="/">
